2a08a7dbf7af88515e497f8a75567e189f470a08
Kelsey Pedersen <kelseypedersen@gmail.com>
2015-07-20 20:46:22 -0700

    Initial Commit

---
+++ ShopStyle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShopStyle
+//
+//  Created by Kelsey Pedersen on 7/20/15.
+//  Copyright (c) 2015 Kelsey Pedersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
